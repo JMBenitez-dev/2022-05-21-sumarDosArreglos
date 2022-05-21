e84ea540fe67c8a8f7df8857c3d90c01ebd85025
FernandoTDF <104107236+FernandoTDF@users.noreply.github.com>
2022-05-21 19:46:11 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+let arregloA:number[]=new Array(6);
+let arregloB:number[]=new Array(6);
+let arregloSuma:number[]=new Array(6);
+
+let indice:number;
+let sumaElementos:number = 0;
+
+for (indice=0; indice < arregloA.length ; indice++){
+    arregloA[indice] = Number(prompt(`Ingrese el valor del elemento actual ${indice} de ${arregloA.length} del arreglo A`));
+  ;}
+
+for (indice=0; indice < arregloB.length ; indice++){
+  arregloB[indice] = Number(prompt(`Ingrese el valor del elemento actual ${indice} de ${arregloB.length} del arreglo B`));
+;}
+
+for (indice=0; indice < arregloSuma.length ; indice++){
+  arregloSuma[indice] = arregloA[indice] + arregloB[indice];
+  console.log(`La suma del elemento ${indice} del arreglo A:${arregloA[indice]} + el del arreglo B:${arregloB[indice]} = ${arregloSuma[indice]}`);
+}
